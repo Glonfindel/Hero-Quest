3440e38dda0b531f419efbfc90dfc8b5ec601dc6
Glonfindel <glonfindel1995@gmail.com>
2018-04-14 12:01:16 +0200

    Basic movement

---
+++ Assets/MapPoint.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPoint : MonoBehaviour {
+
+    public List<MapPoint> Destinations;
+
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public MapPoint currentLocation;
+
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                if (currentLocation.Destinations.Contains(hit.collider.GetComponent<MapPoint>()))
+                {
+                    transform.position = hit.collider.transform.position;
+                    currentLocation = hit.collider.GetComponent<MapPoint>();
+                }
+            }
+        }
+    }
+}
